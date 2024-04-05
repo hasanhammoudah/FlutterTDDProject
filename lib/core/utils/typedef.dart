25102a69711769c4eea9c05ff3641f5e6ca6a9a3
hasan <hassun.h98@gmail.com>
2024-04-05 22:43:11 +0300

    Add UserModel and User model test with fixtures
    Add UserModel class that extends User entity with fromMap, fromJson,
    copyWith, toMap, and toJson methods. Also, add tests for UserModel
    class including fromMap, fromJson, toMap, and toJson methods with
    fixture data.

--- lib/core/utils/typedef.dart
+++ lib/core/utils/typedef.dart
@@ -3,3 +3,4 @@ import 'package:tdd/core/errors/failure.dart';
 
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
 typedef ResultVoid<T> = ResultFuture<void>;
+typedef DataMap = Map<String, dynamic>;
